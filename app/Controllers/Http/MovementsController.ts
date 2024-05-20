4c4190ca18586ce04c456907ff887f60d261e96e
Emanuel-wp <emanuelwp0@gmail.com>
2024-05-20 18:42:50 -0300

    Movements CRUD

---
+++ app/Controllers/Http/MovementsController.ts
@@ -0,0 +1,79 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import Movement from "App/Models/Movement";
+import MovementType from "App/Models/MovementType";
+import Product from "App/Models/Product";
+import { DateTime } from "luxon";
+
+export default class MovementsController {
+  public async index({}: HttpContextContract) {
+    const movements = await Movement.all();
+    return movements;
+  }
+
+  public async store({ request, auth }: HttpContextContract) {
+    const body = request.body();
+    const user = await auth.authenticate();
+    const movementType = await MovementType.findOrFail(body.movementTypeId);
+
+    const movementQuantity = parseInt(body.movementQuantity, 10);
+
+    if (movementType?.name === "Entrada") {
+      const product = await Product.findOrFail(body.productId);
+      product.stockQuantity = product.stockQuantity + movementQuantity;
+      product.save();
+    } else if (movementType?.name === "Transferência") {
+      const product = await Product.findOrFail(body.productId);
+      product.stockQuantity = product.stockQuantity - movementQuantity;
+      product.showcaseQuantity = product.showcaseQuantity + movementQuantity;
+      product.save();
+    } else if (movementType?.name === "Saída") {
+      const product = await Product.findOrFail(body.productId);
+      product.showcaseQuantity = product.showcaseQuantity - movementQuantity;
+      product.save();
+    } else if (movementType?.name === "Descarte") {
+      const product = await Product.findOrFail(body.productId);
+      product.stockQuantity = 0;
+      product.showcaseQuantity = 0;
+      product.save();
+    }
+
+    const date = DateTime.now();
+    const dateFormatted = date.toFormat("dd/MM/yyyy HH:mm:ss");
+
+    const movement = await Movement.create({
+      movementQuantity: body.movementQuantity,
+      dateTime: body.dateTime || dateFormatted,
+      description: body.description,
+      userId: user.id,
+      productId: body.productId,
+      supplierId: body.supplierId,
+      movementTypeId: body.movementTypeId,
+    });
+    return movement;
+  }
+
+  public async show({ request }: HttpContextContract) {
+    const movementId = request.param("id");
+    const movement = await Movement.query().where("id", movementId);
+    return movement;
+  }
+
+  // public async update({ request }: HttpContextContract) {
+  //   const movementId = request.param("id");
+  //   const body = request.body();
+  //   const movement = await Movement.findOrFail(movementId);
+  //   await movement.merge(body).save();
+  //   return movement;
+  // }
+
+  // public async destroy({ request }: HttpContextContract) {
+  //   const movementId = request.param("id");
+  //   const movement = await Movement.findOrFail(movementId);
+  //   await movement.delete();
+  //   return `Movement ${movementId} was deleted`;
+  // }
+
+  // public async edit({}: HttpContextContract) {}
+
+  // public async create({}: HttpContextContract) {}
+}
